aef9804f0186e9c1b8a7d8935a9b67f9b46d6d3a
Ankita Kumari <Ankitak28@gmail.com>
2018-10-07 14:14:21 -0700

    Google and FB auth added

--- app/src/main/java/com/gs/eventmanager/HomeActivity.java
+++ app/src/main/java/com/gs/eventmanager/HomeActivity.java
@@ -5,11 +5,42 @@ import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.GoogleBuilder().build(),
+            new AuthUI.IdpConfig.FacebookBuilder().build());
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
 
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == RC_SIGN_IN) {
+                IdpResponse response = IdpResponse.fromResultIntent(data);
+
+                if (resultCode == RESULT_OK) {
+                    // Successfully signed in
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    // ...
+                } else {
+                    // Sign in failed. If response is null the user canceled the
+                    // sign-in flow using the back button. Otherwise check
+                    // response.getError().getErrorCode() and handle the error.
+                    // ...
+                    Toast.makeText(this,"Unable to Login",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        }
 
 
     }
